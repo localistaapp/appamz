919611c382161f8178d9841f974610d16e2ebd2a
Srishti S <srishti@Srishtis-MacBook-Air.local>
2025-04-21 19:06:59 +0530

    vhost commit

--- Server.js
+++ Server.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOMServer from "react-dom/server";
 import path from "path";
 import AppSSR from "./ssr-client/src/AppSSR";
+import StoreSSR from "./ssr-client/src/store/StoreSSR";
 import express from "express";
 import fs from "fs";
+const vhost = require('vhost');
 
 const app = express();
 const port = 3009;
@@ -32,7 +34,28 @@ const ReadDirectoryContentToArray = (folderPath, array) => {
 ReadDirectoryContentToArray(`${staticPathRoot}/js`, bootstrapScripts);
 ReadDirectoryContentToArray(`${staticPathRoot}/css`, bootstrapCSS);
 
-app.get("/$", (req, res) => {
+app.get("/dashboard", (req, res) => {
+  res.socket.on("error", (error) => console.log("Fatal error occured", error));
+
+  let didError = false;
+  const stream = ReactDOMServer.renderToPipeableStream(
+    <StoreSSR bootStrapCSS={bootstrapCSS} />,
+    {
+      bootstrapScripts,
+      onShellReady: () => {
+        res.statusCode = didError ? 500 : 200;
+        res.setHeader("Content-type", "text/html");
+        stream.pipe(res);
+      },
+      onError: (error) => {
+        didError = true;
+        console.log("Error", error);
+      },
+    }
+  );
+});
+
+app.get("/", (req, res) => {
   res.socket.on("error", (error) => console.log("Fatal error occured", error));
 
   let didError = false;
@@ -57,6 +80,14 @@ app.use(
   "/ssr-client/build/static",
   express.static(__dirname + "/ssr-client/build/static")
 );
+
+app.use(
+  "/app/",
+  express.static(path.join(__dirname, 'app'))
+);
+
+app.use(vhost('kindjpnagar.amuzely.com', express.static(path.join(__dirname, '/app/blr/kindjpnagar'))))
+.use(vhost('urbansareesbroad.amuzely.com', express.static(path.join(__dirname, '/app/blr/urbansareesbroad'))));
 
 app.listen(port, () => {
     console.log(`Application started on port ${port}`);
---
+++ ssr-client/src/store/StoreSSR.js
@@ -0,0 +1,29 @@
+import Dashboard from "./Dashboard";
+import PropTypes from 'prop-types';
+
+const StoreSSR = ({ bootStrapCSS=[] }) => {
+    console.log('Rendering Store App component on server-side');
+    return (
+        <html>
+            <head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Amuzely | Store Dashboard</title>
+                {
+                    bootStrapCSS.map(cssPath => <link key={cssPath} rel="stylesheet" href={cssPath}></link>)
+                }
+            </head>
+            <body>
+                <div id="root-store">
+                    <Dashboard />
+                </div>
+            </body>
+        </html>
+    )
+}
+
+StoreSSR.propTypes = {
+    bootStrapCSS: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default StoreSSR;
