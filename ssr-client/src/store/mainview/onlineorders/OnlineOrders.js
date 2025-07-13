99f7c9ce08d5b42f29e14feb96fe0cd1708ef1e7
Srishti S <srishti@Srishtis-MacBook-Air.local>
2025-07-13 13:50:08 +0530

    Dashboard yp commit

--- ssr-client/src/store/mainview/onlineorders/OnlineOrders.js
+++ ssr-client/src/store/mainview/onlineorders/OnlineOrders.js
@@ -50,7 +50,7 @@ const OrdersList = ({orders}) => {
       const supportMob = JSON.parse(prof).supportMobile;
       axios.post(`/store/web-order/update`, {onlineOrderId: orderId, trackingLink: 'www.slimcrust.com/yp.html?oid='+orderId+'&amt='+orderPrice+'&mob='+orderMobile+'&supportMob='+supportMob}).then((response) => {
         console.log(response.status);
-        window.open(`https://wa.me/+91${orderMobile}?text=Hello!%20Your%20order%20has%20been%20accepted%20by%20${storeName}!%20Please%20make%20your%20payment%20on%20the%20app%20to%20confirm%20at%20${orderFromUrl}?track=true`,'_blank');
+        window.open(`https://wa.me/+91${orderMobile}?text=Hello!%20Your%20order%20has%20been%20accepted%20by%20${storeName}!%20Please%20make%20your%20payment%20on%20the%20app%20to%20confirm%20at%20${orderFromUrl}`,'_blank');
       });
     }
  
