2d6819b9c561d91a8221a509fe3932a32802358c
Srishti S <srishti@Srishtis-MacBook-Air.local>
2025-07-28 23:21:28 +0530

    qr commit

--- ssr-client/src/app/mainview/viewshops/ViewShopsApp.js
+++ ssr-client/src/app/mainview/viewshops/ViewShopsApp.js
@@ -67,7 +67,7 @@ const StoreDetail = ({showView, product, slideRight, onBackClick}) => {
         <span class="store-title">{product && product.name}</span>
 	        <div style={{marginTop: '12px'}} className="bg-white shadow-lg rounded-xl overflow-hidden h-[400px] md:h-[500px]">
 	          <iframe 
-	            src={`https://www.google.com/maps?q=${product && product.geometry.location.lat},${product && product.geometry.location.lng}&amp;z=14&amp;output=embed`}
+	            src={`https://www.google.com/maps?key=AIzaSyA38gnkeYsgyTgs4vAXt2r10Vlgg1R2-ec&q=${product && product.geometry.location.lat},${product && product.geometry.location.lng}z=15&output=embed`}
 	            className="w-full h-full border-0"
 	            loading="lazy"
 	            referrerPolicy="no-referrer-when-downgrade"
