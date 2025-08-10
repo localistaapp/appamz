0b8c953a5def32152bbe1723eb41c3c9aa60be1c
Srishti S <srishti@Srishtis-MacBook-Air.local>
2025-08-10 15:30:38 +0530

    viral deals commit

--- ssr-client/src/app/header/Header.js
+++ ssr-client/src/app/header/Header.js
@@ -79,7 +79,7 @@ const Header = (props) => {
 
     return <div
         className={`${!isShopFlow && cashbackExists ? 'top-margin' : ''} header`}>
-            {!isShopFlow && cashbackExists && <span className="announcement">You're eligible for cashback upto ₹{cashbackValue}. Shop now to redeem!</span>}
+            {!isShopFlow && cashbackExists && <span className="announcement">🎉 You're eligible for instant cashback of ₹{cashbackValue}. Shop now to redeem!</span>}
             {!isShopFlow && <svg onClick={props.showSideBar} className={`${!isShopFlow && cashbackExists ? 'top-margin' : ''} menu-bar text-indigo-600`} width="41" height="41" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">                <rect width="56" height="56" rx="16" fill="#b9b9b9"></rect>                <path d="M37 32H19M37 24H19" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>              </svg>}
             {!isShopFlow && <img onClick={()=> window.location.href=homeLocation} className={`${!isShopFlow && cashbackExists ? 'top-margin-sm' : ''} store-logo-sm`} src={logoSrc} />}
             <img class={isShopFlow ? `${!isShopFlow && cashbackExists ? 'top-margin' : ''} store-logo-shop` :`${!isShopFlow && cashbackExists ? 'top-margin' : ''} store-logo`} src="../../assets/images/qlogo.png"></img>
