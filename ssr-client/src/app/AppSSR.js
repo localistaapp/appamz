38c0efd10999a8453200e19bc5d58a7ba0a31b87
Srishti S <srishti@Srishtis-MacBook-Air.local>
2025-08-15 23:15:54 +0530

    viral deals commit

--- ssr-client/src/app/AppSSR.js
+++ ssr-client/src/app/AppSSR.js
@@ -17,8 +17,12 @@ const AppSSR = ({ pathName='swirlyojpnagar', appName = 'quickrush', bootStrapCSS
 
     const getPushScript = (pathName) => {
         switch (pathName) {
-            case 'swirlyojpnagar': return eval('!function(e,t){var a=e.createElement(t),n=e.getElementsByTagName(t)[0];a.src="https://cdn.pushalert.co/integrate_c86ff931de39cf0d79d8e9edbe367fb3.js",n.parentNode.insertBefore(a,n)}(document,"script");');
-            case 'kidsaurajpnagar': return eval('!function(e,t){var a=e.createElement(t),n=e.getElementsByTagName(t)[0];a.src="https://cdn.pushalert.co/integrate_814b4581392c255b71888d7c8adda385.js",n.parentNode.insertBefore(a,n)}(document,"script");');
+            case 'swirlyojpnagar':  if (typeof window !== 'undefined'){
+                return eval('!function(e,t){var a=e.createElement(t),n=e.getElementsByTagName(t)[0];a.src="https://cdn.pushalert.co/integrate_c86ff931de39cf0d79d8e9edbe367fb3.js",n.parentNode.insertBefore(a,n)}(document,"script");');
+            } 
+            case 'kidsaurajpnagar':if (typeof window !== 'undefined'){
+                 return eval('!function(e,t){var a=e.createElement(t),n=e.getElementsByTagName(t)[0];a.src="https://cdn.pushalert.co/integrate_814b4581392c255b71888d7c8adda385.js",n.parentNode.insertBefore(a,n)}(document,"script");');
+            }
         }
         return <script src="https://cdn.pushalert.co/integrate_814b4581392c255b71888d7c8adda385.js"></script>;
     }
