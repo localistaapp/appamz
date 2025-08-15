b1924ed2694cd9b52d0ef0663e01c5f299faee60
Srishti S <srishti@Srishtis-MacBook-Air.local>
2025-08-15 17:47:36 +0530

    viral deals commit

---
+++ assets/scripts/pa.js
@@ -0,0 +1,6 @@
+(function(d, t) {
+    var g = d.createElement(t);
+    var s = d.getElementsByTagName(t)[0]; // declare s
+    g.src = "https://cdn.pushalert.co/integrate_adbb124b37b784e840ee259d38d4cef0.js";
+    s.parentNode.insertBefore(g, s);
+})(document, "script");
--- ssr-client/src/app/AppSSR.js
+++ ssr-client/src/app/AppSSR.js
@@ -31,7 +31,7 @@ const AppSSR = ({ pathName='swirlyojpnagar', appName = 'quickrush', bootStrapCSS
                 {
                     cssPaths.map(cssPath => { return <link key={cssPath} rel="stylesheet" href={cssPath}></link>})
                 }
-                <script type="text/javascript" src="./js/pa.js"></script>
+                <script type="text/javascript" src="../assets/js/pa.js"></script>
             </head>
             <body>
                 <div id="root-app">
