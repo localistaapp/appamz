f2f3e695d14297c64387562ac19302c7d4febcd8
Srishti S <srishti@Srishtis-MacBook-Air.local>
2026-03-07 17:19:24 +0530

    check hydration

--- ssr-client/src/shop/ShopDashboard.js
+++ ssr-client/src/shop/ShopDashboard.js
@@ -31,33 +31,6 @@ const LoadingSidebarScreen = () => <div className="loading-screen"><div classNam
 const LoadingFooterScreen = () => <div>Loading Footer...</div>;
 const LoadingCarDetailScreen = () => <div>Loading Car Details...</div>;
 
-const initializeStats = (email) => {
-    let enquiriesArr = [];
-    typeof window !== 'undefined' && window.sessionStorage.setItem('user-profile', email);
-    axios.get(`/stats/${email}`)
-      .then(function (response) {
-        if(response.data != 'auth error') {
-            typeof window !== 'undefined' && window.sessionStorage.setItem('user', JSON.stringify(email));
-            if (response.data) {
-                window.sessionStorage.setItem('user-profile', '{"user":"'+email+'","storeId":"'+response.data.storeId+'","franchiseId":"'+response.data.franchiseId+'"}');
-                /*this.setState({
-                  statTotalSales: response.data.eventSales && response.data.storeSales?  new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', minimumFractionDigits: 0 }).format(response.data.eventSales + response.data.storeSales): 0,
-                  statEventSales: response.data.eventSales? new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', minimumFractionDigits: 0 }).format(response.data.eventSales) : 0,
-                  statStoreSales: response.data.storeSales? new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', minimumFractionDigits: 0 }).format(response.data.storeSales) : 0,
-                });*/
-            }
-            document.getElementById('dash-content').style.display='block';
-            document.getElementById('logout').style.display='block';
-        }
-      }.bind(this));
-
-      const dayName = new Date().toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase();
-      axios.get(`/content-lookup/${dayName}`)
-      .then(function (response) {
-        console.log('---content-lookup-response---', response.data);
-      }.bind(this));
-}
-
 function ShopDashboard({storeConfig, locationHref}) {
     const [selectedCar, setSelectedCar] = useState(null);
     const [showSideBar, setShowSideBar] = useState(false);
