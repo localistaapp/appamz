6c2eaf7dd49845a974786166800cbb7d3bc1adbb
Srishti S <srishti@Srishtis-MacBook-Air.local>
2026-03-07 17:57:42 +0530

    check hydration

--- ssr-client/src/store/StoreSSR.js
+++ ssr-client/src/store/StoreSSR.js
@@ -8,6 +8,7 @@ const StoreSSR = ({ bootStrapCSS=[], locationHref='' }) => {
         <html>
             <head>
                 <meta charSet="utf-8" />
+                <meta name="format-detection" content="telephone=no"></meta>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
