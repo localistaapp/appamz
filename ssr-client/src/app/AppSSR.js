0c085a312e4a0a781f8d230d03c0c909e8ae0033
Srishti S <srishti@Srishtis-MacBook-Air.local>
2026-03-15 16:42:13 +0530

    fix app

--- ssr-client/src/app/AppSSR.js
+++ ssr-client/src/app/AppSSR.js
@@ -27,9 +27,6 @@ const AppSSR = ({ pathName='swirlyojpnagar', appName = 'quickrush', bootStrapCSS
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
                 <link href="https://fonts.googleapis.com/css?family=Quicksand:100..900" rel="stylesheet"></link>
                 <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet"></link>
-                <meta property="og:title" content="Special offer on Slashify 🎉"/>
-                <meta property="og:type" content="website"/>
-                <meta property="og:image" content="https://wishler.in/assets/images/deals.png"/>
                 <link rel="manifest" href="/manifest.json" />
                 <title>{`Lootler - deals on your favourites`}</title>
                 <style type="text/css">
